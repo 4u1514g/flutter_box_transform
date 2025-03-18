88eeb07dc9ce5f8bd9f2a8f6b0b9f3fa04562405
duong <duong290199@gmail.com>
2025-03-18 14:28:59 +0700

    a

--- lib/flutter_box_transform.dart
+++ lib/flutter_box_transform.dart
@@ -1,6 +1,7 @@
 library;
 
 export 'src/extensions.dart';
+export 'src/box_transform/enums.dart';
 export 'src/handles.dart';
 export 'src/transformable_box.dart';
 export 'src/transformable_box_controller.dart';
